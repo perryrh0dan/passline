c370bee2a8b2323e3b34adcf064cc9262157e499
Thomas Pöhlmann <thomaspoehlmann96@googlemail.com>
2020-01-08 15:27:12 +0100

    Update generate password method to ensure better passwords

--- pkg/crypt/crypt.go
+++ pkg/crypt/crypt.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"io"
 	random "math/rand"
-	"strings"
 	"time"
 
 	"golang.org/x/crypto/pbkdf2"
@@ -80,15 +79,35 @@ func GenerateKey(password []byte) []byte {
 }
 
 func GeneratePassword(length int) (string, error) {
+	lowercase := []rune("abcdefghijklmnopqrstuvwxyz")
+	uppercase := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	numbers := []rune("0123456789")
+	symbols := []rune("!$%&()/?")
+	all := append(lowercase, uppercase...)
+	all = append(all, numbers...)
+	all = append(all, symbols...)
 	random.Seed(time.Now().UnixNano())
-	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-		"abcdefghijklmnopqrstuvwxyz" +
-		"0123456789" +
-		"!$%&()/?")
-	var b strings.Builder
-	for i := 0; i < length; i++ {
-		b.WriteRune(chars[random.Intn(len(chars))])
+	var a = []rune{}
+
+	// get the requirements
+	a = append(a, lowercase[random.Intn(len(lowercase))])
+	a = append(a, uppercase[random.Intn(len(uppercase))])
+	a = append(a, numbers[random.Intn(len(numbers))])
+	a = append(a, symbols[random.Intn(len(symbols))])
+
+	// populate the rest with random chars
+	for i := 0; i < length-4; i++ {
+		a = append(a, all[random.Intn(len(all))])
 	}
-	password := b.String() // E.g. "ExcbsVQs"
+
+	// shuffle up
+	for i := 0; i < length; i++ {
+		randomPosition := random.Intn(length)
+		temp := a[i]
+		a[i] = a[randomPosition]
+		a[randomPosition] = temp
+	}
+
+	password := string(a)
 	return password, nil
 }
