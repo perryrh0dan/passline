f69a57bcf5639fa3d0023c2ab14a1a1d7f46f073
perryrh0dan <thomaspoehlmann96@googlemail.com>
2020-06-17 13:10:59 +0200

    fix(version): increment version

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ const (
 
 var (
 	// Version is the released version of passline
-	version string = "1.0.0"
+	version string = "1.0.1"
 	// BuildTime is the time the binary was built
 	date string
 )
