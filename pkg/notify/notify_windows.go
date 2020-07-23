3c6717a5e40cd1f4cec592d02d46656334254a00
perryrh0dan <thomaspoehlmann96@googlemail.com>
2020-07-23 15:40:32 +0200

    feat(notify_windows): use windows 10 push notifications

--- pkg/notify/notify_windows.go
+++ pkg/notify/notify_windows.go
@@ -8,13 +8,51 @@ import (
 	"os/exec"
 
 	"passline/pkg/ctxutil"
+
+	"golang.org/x/sys/windows/registry"
+	"gopkg.in/toast.v1"
 )
+
+var isWindows10 bool
+
+func init() {
+	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Microsoft\Windows NT\CurrentVersion`, registry.QUERY_VALUE)
+	if err != nil {
+		return
+	}
+	defer k.Close()
+
+	maj, _, err := k.GetIntegerValue("CurrentMajorVersionNumber")
+	if err != nil {
+		return
+	}
+
+	isWindows10 = maj == 10
+}
 
 // Notify displays a desktop notification through msg
 func Notify(ctx context.Context, subj, msg string) error {
 	if os.Getenv("PASSLINE_NO_NOTIFY") != "" || !ctxutil.IsNotifications(ctx) {
 		return nil
 	}
+
+	if isWindows10 {
+		return sendBaloon(subj, msg)
+	} else {
+		return sendToast(subj, msg)
+	}
+}
+
+func sendToast(subj, msg string) error {
+	notification := toast.Notification{
+		AppID:   "Passline",
+		Title:   subj,
+		Message: msg,
+	}
+	return notification.Push()
+}
+
+func sendBaloon(subj, msg string) error {
 	winmsg, err := exec.LookPath("msg")
 	if err != nil {
 		return err
